76b3b74eb23e7ba5b12415066ce3a9bb2d1d4b2c
chenlitao <269391704@qq.com>
2016-05-20 10:09:28 +0800

    init new project

---
+++ MediaProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MediaProject
+//
+//  Created by CLT on 16/5/20.
+//  Copyright © 2016年 yaxin.guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MediaProject/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MediaProject
+//
+//  Created by CLT on 16/5/20.
+//  Copyright © 2016年 yaxin.guo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
