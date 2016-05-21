cd0a5aa49f094bc8019658c8f5087b022be65aa8
chenlitao <269391704@qq.com>
2016-05-21 10:12:12 +0800

    AFNetworking 更新

---
+++ MediaProject/NetWorkBase.h
@@ -0,0 +1,12 @@
+//
+//  NetWorkBase.h
+//  MediaProject
+//
+//  Created by CLT on 16/5/20.
+//  Copyright © 2016年 yaxin.guo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@interface NetWorkBase : NSObject
+
+@end
---
+++ MediaProject/NetWorkBase.m
@@ -0,0 +1,13 @@
+//
+//  NetWorkBase.m
+//  MediaProject
+//
+//  Created by CLT on 16/5/20.
+//  Copyright © 2016年 yaxin.guo. All rights reserved.
+//
+
+#import "NetWorkBase.h"
+
+@implementation NetWorkBase
+
+@end
